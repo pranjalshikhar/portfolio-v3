2c6b595b45ac0977e08c9230ce0a4557c2c7d44d
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-04-23 13:33:08 +0530

    ⚙️ updated tech cards

--- pages/tech.jsx
+++ pages/tech.jsx
@@ -14,136 +14,109 @@ const tech = () => {
       title: "VS Code",
       id: 0,
       main: "/icons/vscode-original.svg",
-      desc: "Best coding IDE to ever exist,comes with tons of customization",
+      desc: "Best coding IDE to ever exist, comes with tons of customization",
       tag: "code",
       bg: "bg-blue-50",
       text: "text-blue-500",
     },
     {
-      title: "Behance",
+      title: "JavaScript",
       id: 1,
-      main: "/icons/behance-original.svg",
-      desc: "A frontend framework to develop applications.",
-      tag: "code",
-      bg: "bg-emerald-50",
-      text: "text-emerald-700",
-    },
-    {
-      title: "Figma",
-      id: 2,
-      main: "/icons/figma-original.svg",
-      desc: "Discord ultimate hangout spot, home to many bots such as midjourney ",
-      tag: "daily",
-      bg: "bg-emerald-50",
-      text: "text-emerald-700",
-    },
-    {
-      title: "Brave",
-      id: 3,
-      main: "brave-icon.svg",
-      desc: "A world of beauty and serenity, crafted and accompanied by soothing music ",
-      tag: "zen",
-      bg: "bg-purple-100",
-      text: "text-purple-500",
-    },
-    {
-      title: "Git",
-      id: 4,
-      main: "/icons/git-original-wordmark.svg",
-      desc: `A developer's go-to platform for collaborating and showcasing their code`,
-      tag: "code",
-      bg: "bg-blue-50",
-      text: "text-blue-500",
-    },
-    {
-      title: "GitHub",
-      id: 5,
-      main: "/icons/github-original.svg",
-      desc: "The GOAT metaframework for reactJs that comes with SSR and other features",
-      tag: "code",
-      bg: "bg-blue-50",
-      text: "text-blue-500",
-    },
-    {
-      title: "Osu!",
-      id: 6,
       main: "/icons/javascript-original.svg",
-      desc: "OG F2P anime rhythm game coming way back from 2007, one of my favorite ",
-      tag: "zen",
-      bg: "bg-purple-100",
-      text: "text-purple-500",
+      desc: "A lightweight, interpreted, or just-in-time compiled programming language with first-class functions.",
+      tag: "code",
+      bg: "bg-yellow-50",
+      text: "text-yellow-500",
     },
     {
-      title: "Typescript",
-      id: 7,
-      main: "/typescript-icon.svg",
-      desc: "SuperScript of Javascript, made by microsoft with robust type safety",
+      title: "TypeScript",
+      id: 2,
+      main: "/icons/typescript-plain.svg",
+      desc: "SuperScript of Javascript, made by microsoft with robust type safety.",
       tag: "code",
       bg: "bg-blue-50",
       text: "text-blue-500",
     },
     {
-      title: "Figma",
-      id: 8,
-      main: "/figma-icon.svg",
-      desc: "A boon for webapp designers, from wireframe to animation, it has it all ",
+      title: "React JS",
+      id: 3,
+      main: "/icons/react-original.svg",
+      desc: "A free and open-source front-end JavaScript library for building user interfaces based on components",
+      tag: "develop",
+      bg: "bg-blue-50",
+      text: "text-blue-500",
+    },
+    {
+      title: "Next JS",
+      id: 4,
+      main: "/icons/nextjs-original.svg",
+      desc: "An open-source web development framework providing React-based web applications.",
+      tag: "develop",
+      bg: "bg-gray-900",
+      text: "text-gray-50",
+    },
+    {
+      title: "Tailwind CSS",
+      id: 5,
+      main: "/icons/tailwindcss-plain.svg",
+      desc: "A utility-first CSS framework for rapidly building modern websites without ever leaving your HTML.",
       tag: "design",
-      bg: "bg-orange-100",
-      text: "text-orange-600",
+      bg: "bg-green-50",
+      text: "text-green-500",
+    },
+    {
+      title: "Material UI",
+      id: 6,
+      main: "/icons/materialui-original.svg",
+      desc: "A comprehensive suite of UI tools to help you ship new features faster.",
+      tag: "develop",
+      bg: "bg-indigo-50",
+      text: "text-indigo-500",
+    },
+    {
+      title: "SASS",
+      id: 7,
+      main: "/icons/sass-original.svg",
+      desc: "CSS with superpowers. The most powerful professional grade CSS extension language in the world.",
+      tag: "design",
+      bg: "bg-pink-300",
+      text: "text-pink-800",
     },
     {
       title: "Notion",
-      id: 9,
+      id: 8,
       main: "/notion-icon.svg",
-      desc: "The most used, well designed, versatile and professional productivity app",
-      tag: "daily",
-      bg: "bg-emerald-50",
-      text: "text-emerald-700",
+      desc: "A new tool that blends your everyday work apps into one. It's the all-in-one workspace for you and your team.",
+      tag: "productivity",
+      bg: "bg-gray-900",
+      text: "text-gray-100",
     },
     {
-      title: "jetBrains IDEs",
-      id: 10,
-      main: "/jetbrains-icon.svg",
-      desc: "No one can beat JB in IDE market, especially PyCharm and IntelliJ ",
-      tag: "code",
-      bg: "bg-blue-50",
-      text: "text-blue-500",
-    },
-    {
-      title: "MongoDB",
-      id: 11,
-      main: "/mongodb-icon.svg",
-      desc: "A scalable and reliable No-SQL database solution for professional use",
-      tag: "code",
-      bg: "bg-blue-50",
-      text: "text-blue-500",
-    },
-    {
-      title: "Canva",
-      id: 12,
-      main: "/canva-icon.svg",
-      desc: " Free and with ton of templates, a prefect alternative of photoshop ",
+      title: "Figma",
+      id: 9,
+      main: "/icons/figma-original.svg",
+      desc: "A boon for webapp designers, from wireframe to animation, it has it all.",
       tag: "design",
-      bg: "bg-orange-100",
-      text: "text-orange-600",
+      bg: "bg-orange-500",
+      text: "text-orange-100",
     },
     {
-      title: "Firebase",
-      id: 13,
-      main: "/firebase-icon.svg",
-      desc: "helps developers to build, manage, and grow their apps with ease ",
-      tag: "code",
-      bg: "bg-blue-50",
-      text: "text-blue-500",
+      title: "GitHub",
+      id: 10,
+      main: "/icons/github-original.svg",
+      desc: "A developer's go-to platform for collaborating and showcasing their code.",
+      tag: "platform",
+      bg: "bg-gray-900",
+      text: "text-gray-100",
     },
     {
-      title: "MonkeyType",
-      id: 14,
-      main: "/mt-icon.png",
-      desc: "minimal and powerful tool to track and improve your  typing habits",
-      tag: "zen",
-      bg: "bg-purple-100",
-      text: "text-purple-500",
+      title: "Brave",
+      id: 11,
+      main: "/brave-icon.svg",
+      desc: "A privacy focused browser used to block ads and extra pops.",
+      tag: "platform",
+      bg: "bg-orange-500",
+      text: "text-orange-100",
     },
   ];
   const [idNumber, setIdNumber] = useState(0);
@@ -188,21 +161,19 @@ const tech = () => {
           </button>
           <div className="bg-gradient-to-tr dark:from-[#dbfefe25] dark:via-[#f9f2ff10] dark:to-[#fff2ec20] from-[#dbfefe] via-[#f9f2ff] to-[#fff2ec] flex p-2 py-2 sm:p-3 md:p-4 lg:p-6  rounded-xl sm:rounded-[0.8rem] md:rounded-[1.2rem] lg:rounded-[1.6rem] mx-auto items-center justify-center space-x-2 sm:space-x-3 md:space-x-4 h-24 md:h-max lg:space-x-6 transition-all duration-700 ease-linear ">
             <div className="basis-[30%] lg:basis-[25%] h-16 w-12 sm:h-20 sm:w-20 md:h-28 md:w-28 lg:h-32 lg:w-32 rounded-2xl ">
-              <div className=" flex mx-auto rounded-2xl relative w-full h-full bg-white">
+              <div className="flex mx-auto rounded-2xl relative w-full h-full bg-white">
                 <Image
                   src={data[idNumber].main}
                   alt="tech"
                   fill
-                  sizes="(max-width: 768px) 100vw,
-          (max-width: 1200px) 50vw,
-          33vw"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   className=" select-none p-1 sm:p-2 md:p-3 lg:p-4 h-full w-full flex mx-auto object-contain rounded-2xl "
                 />
               </div>
             </div>
             <div className=" basis-[70%] lg:basis-[75%] mx-auto transition-all duration-500 ">
               <div className="flex mb-1 justify-between items-center ">
-                <h3 className=" text-[0.7rem] sm:text-sm md:text-base lg:text-xl font-semibold font-deca tracking-wide ">
+                <h3 className="text-[0.7rem] sm:text-sm md:text-base lg:text-xl font-semibold font-deca tracking-wide ">
                   {data[idNumber].title}
                 </h3>
                 <div
@@ -223,7 +194,7 @@ const tech = () => {
             <HiOutlineArrowLongRight className="w-5 h-3 sm:w-6 sm:h-4 md:w-8 md:h-5 lg:w-10 lg:h-6 stroke-[3] group-hover:fill-[#dd44eb]" />
           </button>
         </div>
-        <div className=" mt-12 pb-12 grid grid-cols-4 gap-4 sm:gap-5 md:gap-6 lg:gap-8 w-[80%] sm:w-[75%] md:w-[70%] mx-auto items-center justify-items-center justify-center place-content-center ">
+        <div className="mt-12 pb-12 grid grid-cols-3 gap-4 sm:gap-5 md:gap-6 lg:gap-8 w-[80%] sm:w-[75%] md:w-[70%] mx-auto items-center justify-items-center justify-center place-content-center ">
           {data.map(function (obj, i) {
             return (
               <TechCard
