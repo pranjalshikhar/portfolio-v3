367bcc070c0fd3cf8e34daacd11106d7a1d4e2d8
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-07-17 11:12:41 +0530

    Update Experience.jsx

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -11,17 +11,19 @@ const Experience = () => {
       <div className="bg-purple-300 w-36 h-36 rounded-lg p-4 select-none ">
         <div className="bg-purple-400  h-28 w-28 flex-col justify-end mx-auto blur-none z-40 items-center text-center text-xl text-white font-medium rounded-lg p-2">
           <div className="basis-[60%] text-5xl font-black ">
-            <h2 className="text-border mt-1 ">03</h2>
+            <h2 className="text-border mt-1 ">
+              {new Date().getUTCFullYear() - 2020}
+            </h2>
           </div>
           <div className="basis-[30%] text-xs mt-3 font-bold">
-            {" "}
             Years of Experience
           </div>
         </div>
       </div>
       <div className="w-[80%] mx-auto">
         <div className="text-center max-w-2xl mx-auto tracking-normal leading-5 text-base sm:text-[1.1rem] lg:text-lg">
-          During these 3 years, I have developed a strong problem-solving and
+          During these {new Date().getUTCFullYear() - 2020} years of Frontend
+          Development Experience, I have developed a strong problem-solving and
           critical thinking skills, and am able to quickly adapt to new
           technologies and methods of working. Given are some skill-set that I
           have learned and am still actively learning as days goes by
